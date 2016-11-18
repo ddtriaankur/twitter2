f73a371e0915457009f06a843cfdd539d54c75e9
TKOL <ddtriaankur@gmail.com>
2016-11-18 10:05:25 +0530

    dummy keys added

---
+++ keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'dummy_consumer-key',
+  consumer_secret:      'dummy_consumer-secret',
+  access_token:         'dummy_access-token',
+  access_token_secret:  'dummy_access_token-secret',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+}
